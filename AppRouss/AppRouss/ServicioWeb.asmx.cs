157ab43704379848cc40107e79dcc341d4e8da09
Ezequiel Dalaison <edalaison@sempait.com.ar>
2015-04-01 23:38:34 -0300

    Se borra proyecto de servicio web, se crea como pagina en el proyecto de asp

--- AppRouss/AppRouss/ServicioWeb.asmx.cs
+++ AppRouss/AppRouss/ServicioWeb.asmx.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
-using BibliotecaAppRouss.Controladores;
 
-namespace ServiciosWebAppRouss
+namespace AppRouss
 {
+    /// <summary>
+    /// Descripción breve de ServicioWeb
+    /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
     // [System.Web.Script.Services.ScriptService]
-    public class Service1 : System.Web.Services.WebService
+    public class ServicioWeb : System.Web.Services.WebService
     {
         [WebMethod]
         public string LoginUsuario(string mail, string contraseña)
